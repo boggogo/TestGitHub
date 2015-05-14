ddf342d40c59cb089fb0a9866c7a0d7739ba6c0b
boggogo <koemdjiev@gmail.com>
2015-05-14 12:05:39 +0100

    added startButton...

--- app/src/main/java/criminalintent/android/bignerdranch/com/testgithub/MainActivity.java
+++ app/src/main/java/criminalintent/android/bignerdranch/com/testgithub/MainActivity.java
@@ -5,14 +5,17 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
-
+    private Button startButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = (Button) findViewById(R.id.button);
     }
 
 }
